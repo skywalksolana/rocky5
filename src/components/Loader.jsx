9cfab9903f5e508bb5e638c8e37700f08bccdfeb
5rahulmangla <5rahulmangla@gmail.com>
2025-05-02 22:30:51 +0530

    fix

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -2,14 +2,17 @@ import { Html } from "@react-three/drei";
 
 export const Loader = ()=>{
     return(
+        <Html center>
         <div className=" w-24 h-24  flex justify-center items-center relative">
         <div className="absolute inset-0 flex justify-center items-center">
         <div className="loader animate-spin
          w-12 h-12 border-t-4 border-b-4 border-red-500 
          rounded-full">
+             <img className="w-12 h-19 z-10" src="images/logo.png"></img>
          </div>
-         <img className="w-12 h-19 z-10" src="images/logo.png"></img>
+        
         </div>
         </div>
+        </Html>
     )
 };
