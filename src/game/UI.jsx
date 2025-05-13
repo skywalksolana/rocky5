e4a452ab3081cf3d599d3ae8115736e552897db5
5rahulmangla <5rahulmangla@gmail.com>
2025-05-14 00:59:04 +0530

    beta

--- src/game/UI.jsx
+++ src/game/UI.jsx
@@ -20,7 +20,7 @@ export const UI = () => {
       </h2>
       )}
 
-        <a href="https://boop.fun/tokens/DMvGnJyENUhmAUTRKJVJwavUbZ8b3ETLaXi5vUoboop">
+        <a href="https://pump.fun">
         <img src="images/DexLogo.png" className="pointer-events-auto absolute top-8 right-20 w-4 h-4 transition duration-300 hover:scale-110 hover:rotate-5 hover:opacity-80" />
         </a>
 
